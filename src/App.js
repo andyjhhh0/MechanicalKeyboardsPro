9c50086d199cd518732c1381fea2d923fd3f7605
Andy How Hok Hium <ahowh089@uottawa.ca>
2024-07-07 12:58:47 -0400

    attempt to fix 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Build from './Build'
 import Contact from './Contact'
 import LatestNews from './News';
 import Shop from './Shop';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Navbar/>
      <div className="content">
       <Routes>
-        <Route path='/'element={<Home/>} />
+        <Route path='/Home'element={<Home/>} />
         <Route path="/LogIn" element={<LogIn />} />
         <Route path="/Build" element={<Build />} />
         <Route path="/Contact" element={<Contact />} />
