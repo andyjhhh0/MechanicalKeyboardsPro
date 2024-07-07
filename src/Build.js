67bc908d8575e7f3542d3333088d7f5d8ad4efbd
Andy How Hok Hium <ahowh089@uottawa.ca>
2024-07-07 13:34:49 -0400

    bug fixes 4

--- src/Build.js
+++ src/Build.js
@@ -1,11 +1,11 @@
-
+import { Link } from "react-router-dom";
 
 const Build = () => {
   return (
     <div className="build-your-own-keyboard container mt-5">
       <h2>Build Your Own Keyboard</h2>
       <p>Use our interactive tool to customize and build your own mechanical keyboard.</p>
-      <a href="/" className="btn btn-danger">Start Building</a>
+      <li><Link to="/MechanicalKeyboardsPro">Start building</Link></li>
     </div>
   );
 };
